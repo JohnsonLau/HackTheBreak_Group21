96311fbd76d48b5264ac0e76c82e2b6c10cf461b
J.ohnsonlau <yichenglau@gmail.com>
2021-03-13 17:04:51 -0800

    Css changes

--- index.js
+++ index.js
@@ -9,11 +9,44 @@ app.use(express.urlencoded({ extended: false }));
 app.use(ejsLayouts);
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
+app.get('/index', (req, res) => {
   res.render('schedule/index');
+});
+
+app.get('/checklist', (req, res) => {
+  res.render('schedule/checklist');
+});
+
+app.get('/reminders', (req, res) => {
+  res.render('schedule/reminder');
 });
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
